2d182ede13e857a3501551f44d23a5b0f05f239d
Patrick Clarke <ekralccp@me.com>
2017-01-21 17:29:05 -0800

    Got CT displayed

---
+++ js/fetanyl.js
@@ -0,0 +1,28 @@
+var width = 1000,
+    height = 1000;
+
+/*var projection = d3.geoMercator()
+  .scale(50000)
+  .center([41.651880, -72.641602])
+  .translate([width / 2, height / 2]);*/
+
+var projection = d3.geoAlbers()
+  .scale(25000)
+  .translate([-7200, 2450]);
+
+var path = d3.geoPath()
+    .projection(projection);
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+d3.json("data/ct.json", function(error, ct) {
+
+  console.log(ct);
+
+  svg.append("path")
+      .attr("class", "states")
+      .datum(topojson.feature(ct, ct.objects.cb_2015_09_tract_500k))
+      .attr("d", path);
+});
